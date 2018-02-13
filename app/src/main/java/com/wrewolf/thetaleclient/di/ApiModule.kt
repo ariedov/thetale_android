3b053ebd6e4f343f6061c367014c83478ef1956b
David Leibovych <david.leibovych@x2sy.com>
2018-02-13 18:03:37 +0200

    preparing to use additional interceptors

--- app/src/main/java/com/wrewolf/thetaleclient/di/ApiModule.kt
+++ app/src/main/java/com/wrewolf/thetaleclient/di/ApiModule.kt
@@ -2,6 +2,8 @@ package com.wrewolf.thetaleclient.di
 
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.IntoSet
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import org.thetale.api.ClientBuilder
 import org.thetale.api.TheTaleService
@@ -12,19 +14,27 @@ class ApiModule {
 
     @Provides
     @Singleton
-    fun httpClient(): OkHttpClient {
+    @IntoSet
+    fun appVersionInterceptor(): Interceptor {
+        return Interceptor {
+            var request = it.request()
+            val url = request.url()
+                    .newBuilder()
+                    .addQueryParameter("app_version", "value")
+                    .build()
+            request = request.newBuilder().url(url).build()
+            it.proceed(request)
+        }
+    }
 
-        return OkHttpClient.Builder()
-                .addInterceptor {
-                    var request = it.request()
-                    val url = request.url()
-                            .newBuilder()
-                            .addQueryParameter("app_version", "value")
-                            .build()
-                    request = request.newBuilder().url(url).build()
-                    it.proceed(request)
-                }
-                .build()
+    @Provides
+    @Singleton
+    fun httpClient(interceptors: Set<@JvmSuppressWildcards Interceptor>): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            interceptors.forEach {
+                addInterceptor(it)
+            }
+        }.build()
     }
 
     @Provides
