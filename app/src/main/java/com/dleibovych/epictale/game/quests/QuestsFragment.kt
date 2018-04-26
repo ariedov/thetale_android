197d4dbc73113e33d2be46a63d46010a80c7a14d
David Leibovych <david.leibovych@x2sy.com>
2018-04-26 17:30:40 +0300

    removes steps for quest

--- app/src/main/java/com/dleibovych/epictale/game/quests/QuestsFragment.kt
+++ app/src/main/java/com/dleibovych/epictale/game/quests/QuestsFragment.kt
@@ -64,135 +64,111 @@ class QuestsFragment : Fragment(), QuestsView {
         presenter.start()
     }
 
+    override fun showProgress() {
+
+    }
+
     override fun showQuests(info: GameInfo) {
         container!!.removeAllViews()
         actorNames.clear()
-        val questLinesCount = info.account!!.hero.quests.quests.size
-        for (i in 0 until questLinesCount) {
-            val questLine = info.account!!.hero.quests.quests[i]
-            val questStepsCount = questLine.line.size
-            for (j in 0 until questStepsCount) {
-                val questStep = questLine.line[j]
-                val questStepView = layoutInflater!!.inflate(R.layout.item_quest, container, false)
 
-                val questNameView = questStepView.findViewById<View>(R.id.quest_name) as TextView
-                val rewards: String?
-                rewards = if (questStep.experience > 0 && questStep.power == 0) {
-                    String.format(" (%s)", getString(R.string.quest_reward_experience, questStep.experience))
-                } else if (questStep.experience == 0 && questStep.power > 0) {
-                    String.format(" (%s)", getString(R.string.quest_reward_power, questStep.power))
-                } else if (questStep.experience > 0 && questStep.power > 0) {
-                    String.format(" (%s, %s)",
-                            getString(R.string.quest_reward_experience, questStep.experience),
-                            getString(R.string.quest_reward_power, questStep.power))
-                } else {
-                    null
+        val questLine = info.account!!.hero.quests.quests.last()
+        val questStepsCount = questLine.line.size
+        val questStep = questLine.line.last()
+        val questStepView = layoutInflater!!.inflate(R.layout.item_quest, container, false)
+
+        val questNameView = questStepView.findViewById<View>(R.id.quest_name) as TextView
+        val rewards: String?
+        rewards = if (questStep.experience > 0 && questStep.power == 0) {
+            String.format(" (%s)", getString(R.string.quest_reward_experience, questStep.experience))
+        } else if (questStep.experience == 0 && questStep.power > 0) {
+            String.format(" (%s)", getString(R.string.quest_reward_power, questStep.power))
+        } else if (questStep.experience > 0 && questStep.power > 0) {
+            String.format(" (%s, %s)",
+                    getString(R.string.quest_reward_experience, questStep.experience),
+                    getString(R.string.quest_reward_power, questStep.power))
+        } else {
+            null
+        }
+
+        if (TextUtils.isEmpty(rewards)) {
+            questNameView.text = questStep.name
+        } else {
+            val rewardsString = SpannableString(rewards)
+            rewardsString.setSpan(ForegroundColorSpan(resources.getColor(R.color.game_additional_info)),
+                    0, rewardsString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            questNameView.text = TextUtils.concat(questStep.name, rewardsString)
+        }
+
+        (questStepView.findViewById<View>(R.id.quest_icon) as ImageView)
+                .setImageResource(QuestType.getQuestType(questStep.type).drawableResId)
+
+        val actorsContainer = questStepView.findViewById<View>(R.id.quest_actors_container) as ViewGroup
+
+        for (actorInfo in questStep.actors) {
+
+            val actorView = layoutInflater!!.inflate(R.layout.item_text, actorsContainer, false)
+
+            val actorTextView = actorView.findViewById<View>(R.id.item_text_content) as TextView
+            val actorText: CharSequence
+            val actorName = SpannableString(actorInfo.name)
+            actorName.setSpan(StyleSpan(Typeface.BOLD), 0, actorName.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            val actor = actorInfo.actor
+            when (actor) {
+                is QuestActorPersonInfo -> {
+                    actorText = TextUtils.concat(actorName, ": ", actor.name)
+                    actorNames[actorTextView] = actor.place
                 }
 
-                if (TextUtils.isEmpty(rewards)) {
-                    questNameView.text = questStep.name
-                } else {
-                    val rewardsString = SpannableString(rewards)
-                    rewardsString.setSpan(ForegroundColorSpan(resources.getColor(R.color.game_additional_info)),
-                            0, rewardsString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-                    questNameView.text = TextUtils.concat(questStep.name, rewardsString)
+                is QuestActorPlaceInfo -> {
+                    actorText = TextUtils.concat(actorName, ": ", actor.name)
                 }
 
-                (questStepView.findViewById<View>(R.id.quest_icon) as ImageView)
-                        .setImageResource(QuestType.getQuestType(questStep.type).drawableResId)
-//
-                val actorsContainer = questStepView.findViewById<View>(R.id.quest_actors_container) as ViewGroup
-                for (actorInfo in questStep.actors) {
-                    val actorView = layoutInflater!!.inflate(R.layout.item_text, actorsContainer, false)
-
-                    val actorTextView = actorView.findViewById<View>(R.id.item_text_content) as TextView
-                    val actorText: CharSequence
-                    val actorName = SpannableString(actorInfo.name)
-                    actorName.setSpan(StyleSpan(Typeface.BOLD), 0, actorName.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-                    val actor = actorInfo.actor
-                    when (actor) {
-                        is QuestActorPersonInfo -> {
-                            actorText = TextUtils.concat(actorName, ": ", actor.name)
-                            actorNames[actorTextView] = actor.place
-                        }
-
-                        is QuestActorPlaceInfo -> {
-                            actorText = TextUtils.concat(actorName, ": ", actor.name)
-                        }
-
-                        is QuestActorSpendingInfo -> {
-                            actorText = TextUtils.concat(actorName, ": ", actor.goal)
-                        }
-
-                        else -> actorText = actorName
-                    }
-                    actorTextView.text = actorText
-//                    actorTextView.setOnClickListener { DialogUtils.showQuestActorDialog(fragmentManager, actor) }
-
-                    actorsContainer.addView(actorView)
+                is QuestActorSpendingInfo -> {
+                    actorText = TextUtils.concat(actorName, ": ", actor.goal)
                 }
 
-                if (questStep.type !== QuestType.SPENDING.code && j == questStepsCount - 1) {
-                    UiUtils.setText(questStepView.findViewById(R.id.quest_action), questStep.action)
-                    UiUtils.setText(questStepView.findViewById(R.id.quest_current_choice), questStep.choice)
-                }
-
-                if (info.account!!.isOwn) {
-                    val choicesContainer = questStepView.findViewById<View>(R.id.quest_choices_container) as ViewGroup
-                    val choiceProgress = questStepView.findViewById<View>(R.id.quest_choice_progress)
-                    val choiceError = questStepView.findViewById<View>(R.id.quest_choice_error) as TextView
-                    val choices = questStep.choiceAlternatives
-                    if (choices != null) {
-                        for (choice in choices) {
-                            val choiceView = layoutInflater!!.inflate(R.layout.item_quest_choice, choicesContainer, false)
-
-                            val choiceDescription = SpannableString(choice[1])
-                            choiceDescription.setSpan(ForegroundColorSpan(resources.getColor(R.color.common_link)),
-                                    0, choice[1].length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-                            val choiceTextView = choiceView.findViewById<View>(R.id.quest_choice) as TextView
-                            choiceTextView.text = TextUtils.concat(getString(R.string.quest_choice_part), choiceDescription)
-                            choiceTextView.setOnClickListener {
-                                presenter.chooseQuestOption(choice[0].toInt())
-                            }
-
-                            choicesContainer.addView(choiceView)
-                        }
-                    }
-                }
-
-                container!!.addView(questStepView)
+                else -> actorText = actorName
             }
+            actorTextView.text = actorText
 
-            if (i != questLinesCount - 1) {
-                layoutInflater!!.inflate(R.layout.item_quest_delimiter, container, true)
+            actorsContainer.addView(actorView)
+
+            if (info.account!!.isOwn) {
+                val choicesContainer = questStepView.findViewById<View>(R.id.quest_choices_container) as ViewGroup
+                val choiceProgress = questStepView.findViewById<View>(R.id.quest_choice_progress)
+                val choiceError = questStepView.findViewById<View>(R.id.quest_choice_error) as TextView
+                val choices = questStep.choiceAlternatives
+                if (choices != null) {
+                    for (choice in choices) {
+                        val choiceView = layoutInflater!!.inflate(R.layout.item_quest_choice, choicesContainer, false)
+
+                        val choiceDescription = SpannableString(choice[1])
+                        choiceDescription.setSpan(ForegroundColorSpan(resources.getColor(R.color.common_link)),
+                                0, choice[1].length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                        val choiceTextView = choiceView.findViewById<View>(R.id.quest_choice) as TextView
+                        choiceTextView.text = TextUtils.concat(getString(R.string.quest_choice_part), choiceDescription)
+                        choiceTextView.setOnClickListener { presenter.chooseQuestOption(choice[0].toInt()) }
+
+                        choicesContainer.addView(choiceView)
+                    }
+                }
             }
         }
 
-        // add town effectName to quest person actors
-        if (actorNames.size > 0) {
-            //                    new MapRequest(response.mapVersion).execute(RequestUtils.wrapCallback(new CommonResponseCallback<MapResponse, String>() {
-            //                        @Override
-            //                        public void processResponse(MapResponse response) {
-            //                            for (final Map.Entry<TextView, Integer> actorNameEntry : actorNames.entrySet()) {
-            //                                final Spannable placeText = new SpannableString(String.format(" (%s)", response.places.get(actorNameEntry.getValue()).effectName));
-            //                                placeText.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.game_additional_info)),
-            //                                        0, placeText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            //
-            //                                final TextView actorName = actorNameEntry.getKey();
-            //                                actorName.setText(TextUtils.concat(actorName.getText(), placeText));
-            //                            }
-            //                        }
-            //
-            //                        @Override
-            //                        public void processError(String error) {
-            //                            // do nothing
-            //                        }
-            //                    }, QuestsFragment.this));
-        }
+        container!!.addView(questStepView)
     }
 
     override fun showError() {
+
+    }
+
+    override fun showQuestActionProgress() {
+
+    }
+
+    override fun showQuestActionError() {
 
     }
 
--- app/src/main/java/com/dleibovych/epictale/game/quests/QuestsView.kt
+++ app/src/main/java/com/dleibovych/epictale/game/quests/QuestsView.kt
@@ -4,7 +4,13 @@ import org.thetale.api.models.GameInfo
 
 interface QuestsView {
 
+    fun showProgress()
+
     fun showQuests(info: GameInfo)
 
     fun showError()
+
+    fun showQuestActionProgress()
+
+    fun showQuestActionError()
 }
